506a2c32eb3ff43c11429b6d9cd22b3d965ee34e
Nills Franssens <nills.franssens@gmail.com>
2021-03-19 23:20:23 -0700

    Update app.py

--- 4_container_ci-cd-kubernetes/app.py
+++ 4_container_ci-cd-kubernetes/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello world!"
+    return "Hello world, this is the final demo of this presentation!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8001, debug=True)
