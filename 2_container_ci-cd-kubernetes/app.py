601ce92d9765452c6db1bf24330c5ae67d4519bf
Nills Franssens <nills.franssens@gmail.com>
2021-05-21 06:23:55 -0700

    Update app.py

--- 2_container_ci-cd-kubernetes/app.py
+++ 2_container_ci-cd-kubernetes/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello world, this is another demo."
+    return "Hello world, we are running this from a CD pipeline in GitHub Actions."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8001, debug=True)
