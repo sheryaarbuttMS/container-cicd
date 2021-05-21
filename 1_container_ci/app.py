cd5e8438d938447a5b8652af2c0f43175c1fcc6b
Nills Franssens <nills.franssens@gmail.com>
2021-05-21 05:31:01 -0700

    Update app.py

--- 1_container_ci/app.py
+++ 1_container_ci/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello Romania from the live pipeline demo!"
+    return "Hello world from the live pipeline demo!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8001, debug=True)
