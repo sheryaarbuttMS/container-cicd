4c29f26124f8b447be5a29e2c44d25378faf0e9d
Nills Franssens <nills.franssens@gmail.com>
2021-10-09 09:56:47 -0400

    Update app.py

--- 2_container_ci-cd-kubernetes/app.py
+++ 2_container_ci-cd-kubernetes/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Making sure this demo still works."
+    return "Hello Atlanta Code Camp!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8001, debug=True)
