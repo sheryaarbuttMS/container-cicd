81bab7632d6379c9e1d937fd1cc4f65a55e197a9
Nills Franssens <nills.franssens@gmail.com>
2021-11-16 14:56:07 +0100

    Update app.py

--- 1_container_ci/app.py
+++ 1_container_ci/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello Atlanta Code camp! I'm so glad to be outside of the house again."
+    return "Hello Uantwerpen! I'm so glad to be in Belgium again."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8001, debug=True)
